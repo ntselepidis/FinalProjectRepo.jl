859bae1894adcf4f2ee89938b79ca1c436e8a4e8
Romeo Valentin <romeo.valentin@math.ethz.ch>
2021-12-09 14:48:41 +0100

    Update test file
    Actually now we fail for some reason. Even x_g is wrong ?!

--- test/part1.jl
+++ test/part1.jl
@@ -5,7 +5,9 @@ using ParallelStencil
 using ParallelStencil.FiniteDifferences3D
 @init_parallel_stencil(Threads, Float64, 3)
 
-include("../scripts-part1/part1.jl") # modify to include the correct script
+USE_GPU=false
+include("../scripts-part1/part1_kernel_programming.jl") # modify to include the correct script
+include("../scripts-part1/part1_array_programming.jl") # modify to include the correct script
 
 # Reference test using ReferenceTests.jl
 "Compare all dict entries"
@@ -14,14 +16,14 @@ comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (
 MPI.Init()
 Xc_g, H_g = diffusion_3D_array_programming(nx=32, ny=32, nz=32, init_and_finalize_MPI=false)
 inds = Int.(ceil.(LinRange(1, length(Xc_g), 12)))
-d_kernel = Dict(:X=> Xc_g[inds], :H_g=>H_g[inds])
+d_kernel = Dict(:X=> Xc_g[inds], :H_g=>H_g[inds, inds, 15])
 
 Xc_g, H_g = diffusion_3D_array_programming(nx=32, ny=32, nz=32, init_and_finalize_MPI=false)
 inds = Int.(ceil.(LinRange(1, length(Xc_g), 12)))
-d_array = Dict(:X=> Xc_g[inds], :H_g=>H_g[inds])
+d_array = Dict(:X=> Xc_g[inds], :H_g=>H_g[inds, inds, 15])
 MPI.Finalize()
 
 @testset "Ref-file" begin
-    @test_reference "reftest-files/test_1.bson" d_kernel by=comp
     @test_reference "reftest-files/test_1.bson" d_array by=comp
+    @test_reference "reftest-files/test_1.bson" d_kernel by=comp
 end
